70f7b095e9264378a130bb71637d2103dbbaadc2
Fares Edres <faresedres1@gmail.com>
2024-12-20 00:54:43 +0200

    New Features in Queue and Stack

--- DataStructure/Lab1/Linked List/LinkedList.cpp
+++ DataStructure/Lab1/Linked List/LinkedList.cpp
@@ -40,9 +40,25 @@ class LinkedList{
     int insertBefore(int data,int beforeData);
     int getCount();
     int getDataByIndex(int index,int&result);
+    int insertByIndex(int index,int data);
+    void merge(LinkedList& l);
     void display();
     void createHead(int data);
 };
+void LinkedList::merge(LinkedList& l){
+  tail->next=l.head;
+}
+
+int LinkedList::insertByIndex(int index,int data){
+    
+    if(index>size-1)return 0;
+    Node* newNode=new Node(data);
+    if(index==0){
+        newNode->next=head;
+        head=newNode;
+    }
+    return 1;
+    }
 int LinkedList::insertAfter(int data,int afterData){
 Node*newNode=new Node(data);
 Node*curr=searchData(afterData);
@@ -132,40 +148,52 @@ int LinkedList::insertBefore(int data,int beforeData){
         }
 
 int main() {
-    LinkedList list;
-    int test,result;
-    cout << "Insert data:" << endl;
-    cout<<"Insert Before Empty LinkedList"<<endl;
-    test=list.insertBefore(10, 0); 
-    cout<<"Test="<<test<<endl;
-    cout<<"Insert Before After Empty LinkedList"<<endl;
-    test=list.insertAfter(10, 0); 
-    cout<<"Test="<<test<<endl;
-    
+    LinkedList l1,l2;
+    l1.createHead(10);
+    l1.insertAfter(20, 10);
+    l1.insertAfter(30, 20);
+    l1.insertAfter(40, 30);
+    l1.insertBefore(15, 20);
 
+    l2.createHead(10);
+    l2.insertAfter(20, 10);
+    l2.insertAfter(30, 20);
+    l2.insertAfter(40, 30);
+    l2.insertBefore(15, 20);
+    l1.merge(l2);
+    l1.display();
+/*
+    int test, result;
+    cout << "Insert data:" << endl;
+    cout << "Insert Before Empty LinkedList" << endl;
+    test = list.insertBefore(10, 0);
+    cout << "Test=" << test << endl;
+    cout << "Insert Before After Empty LinkedList" << endl;
+    test = list.insertAfter(10, 0);
+    cout << "Test=" << test << endl;
 
     list.createHead(10);
     list.insertAfter(20, 10);
     list.insertAfter(30, 20);
     list.insertAfter(40, 30);
     list.insertBefore(15, 20);
-   
 
-    test=list.getDataByIndex(2,result);
-    if(test==1)cout<<"Index = "<< result<<endl;
-    
+    test = list.getDataByIndex(2, result);
+    if (test == 1)
+        cout << "Index = " << result << endl;
 
     int invalidIndex = 10;
-    test=list.getDataByIndex(invalidIndex,result);
-    if(test==-1) cout << "Accessing invalid index "<<endl;
-    
+    test = list.getDataByIndex(invalidIndex, result);
+    if (test == -1)
+        cout << "Accessing invalid index " << endl;
+
     list.insertBefore(5, 10);
     list.insertAfter(50, 40);
 
-   
     cout << "Final Linked List: ";
     list.display();
 
     cout << "LinkedList Size=" << list.getCount() << endl;
+    */
     return 0;
 }
